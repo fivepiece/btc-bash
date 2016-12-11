2baad308bcca05798223051c1d08ba83e8408240
fivepiece <fivepiece@users.noreply.github.com>
2016-12-11 19:27:24 +0200

    16to2 and 2to16 work well, will refractor started writing tests for shachains renamed hashchains to shachains
    cleanup and debug output using decho

---
+++ test/shachains/test.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+unset i v16 v256 v16to2_256 v16to2_48
+
+#./gen_vectors.sh > vectors.sh
+source ./vectors.sh
+source ../../04_shachains.sh
+
+t16to2()
+{
+    local i v16
+
+    for i in ${!v16to2_48[@]}; do
+
+        read v16 < <( hc_16to2 "${i}" 48 )
+        [[ "${v16}" == "${v16to2_48[$i]}" ]] || echo "t16to2_48, ${v16} != ${v16to2_48[$i]}" #|| return 1
+    done
+
+    unset i v16 
+    
+    for i in ${!v16to2_256[@]}; do
+
+        read v16 < <( hc_16to2 "${i}" 256 )
+        [[ "${v16}" == "${v16to2_256[$i]}" ]] || echo "t16to2_256, ${v16} != ${v16to2_256[$i]}" #|| return 1
+    done
+}
+
+t2to16()
+{
+    local i v256
+
+    for i in ${!v2to16_48[@]}; do
+
+        read v256 < <( hc_2to16 "${i}" 12 )
+        [[ "${v256}" == "${v2to16_48[$i]}" ]] || echo "t2to16_48, ${v256} != ${v2to16_48[$i]}" #|| return 1
+    done
+
+    unset i v256
+    
+    for i in ${!v16to2_256[@]}; do
+
+        read v256 < <( hc_2to16 "${i}" 64 )
+        [[ "${v16}" == "${v2to16_256[$i]}" ]] || echo "t2to16_256, ${v256} != ${v2to16_256[$i]}" #|| return 1
+    done
+}
+
+#unset i i16 v16 v16to2_256 v16to2_48
+#rm ./vectors.sh
